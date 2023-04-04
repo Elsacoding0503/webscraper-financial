381bbda2ad8bd74992639745b24cd2197d2bd475
Elsa Yang <elsayang0503@gmail.com>
2023-04-04 12:25:30 +0800

    converted twse and taifex into csv file

--- taifex_scraper.py
+++ taifex_scraper.py
@@ -1,4 +1,4 @@
-# 證交所twse
+# 台灣期貨taifex
 import requests
 import pandas as pd
 from fake_useragent import UserAgent 
@@ -6,29 +6,42 @@ from fake_useragent import UserAgent
 ua = UserAgent()
 
 headers = {
-    'User-Agent': ua.random
+    'User-Agent': ua.random,
+    'Content-Type': 'application/json;charset=UTF-8'
 }
 
-url = 'https://www.twse.com.tw/rwd/zh/afterTrading/BWIBBU_d?date=20230331&selectType=24&response=json&_=1680575877170'
-response_twse = requests.get(url=url, headers=headers)
-data_twse = response_twse.json()
+url_taifex = 'https://mis.taifex.com.tw/futures/api/getQuoteList'
 
-code = [content_twse[0] for content_twse in data_twse['data']]
-titles = [content_twse[1] for content_twse in data_twse['data']]
-yield_ = [content_twse[2] for content_twse in data_twse['data']]
-dividend_year = [content_twse[3] for content_twse in data_twse['data']]
-pe_ratio = [content_twse[4] for content_twse in data_twse['data']]
-prb = [content_twse[5] for content_twse in data_twse['data']]
-financial_statement_yq = [content_twse[6] for content_twse in data_twse['data']]
+json_data = {
+    'MarketType': '0',
+    'SymbolType': 'F',
+    'KindID': '1',
+    'CID': '',
+    'ExpireMonth': '',
+    'RowSize': '全部',
+    'PageNo': '',
+    'SortColumn': '',
+    'AscDesc': 'A',
+}
 
-semi_con = {}
-semi_con["證券代號"]=code
-semi_con["股票名稱"]=titles
-semi_con["殖利率"]=yield_
-semi_con["股利年度"]=dividend_year
-semi_con["本益比"]=pe_ratio
-semi_con["股價淨值比"]=prb
-semi_con["財報年度年/季"]=financial_statement_yq
+response_taifex = requests.post(url=url_taifex,headers=headers, data=json.dumps(json_data))
+data_taifex = response_taifex.json()
 
-df_semi = pd.DataFrame(semi_con)
-df_semi.to_csv("twse_semi_con.csv",encoding='utf-8', index=False)
+futures_domestic = {}
+futures_domestic["商品"] = [i["DispCName"] for i in data_taifex['RtData']['QuoteList']]
+futures_domestic["買進"] = [i["CBestBidPrice"] for i in data_taifex['RtData']['QuoteList']]
+futures_domestic["買量"] = [i["CBestBidSize"] for i in data_taifex['RtData']['QuoteList']]
+futures_domestic["賣出"] = [i["CBestAskPrice"] for i in data_taifex['RtData']['QuoteList']]
+futures_domestic["賣量"] = [i["CBestAskSize"] for i in data_taifex['RtData']['QuoteList']]
+futures_domestic["成交價"] = [i["CLastPrice"] for i in data_taifex['RtData']['QuoteList']]
+futures_domestic["漲跌"] = [i["CDiff"] for i in data_taifex['RtData']['QuoteList']]
+futures_domestic["振幅％"] = [i["CAmpRate"] for i in data_taifex['RtData']['QuoteList']]
+futures_domestic["成交量"] = [i["CTotalVolume"] for i in data_taifex['RtData']['QuoteList']]
+futures_domestic["開盤"] = [i["COpenPrice"] for i in data_taifex['RtData']['QuoteList']]
+futures_domestic["最高"] = [i["CHighPrice"] for i in data_taifex['RtData']['QuoteList']]
+futures_domestic["最低"] = [i["CLowPrice"] for i in data_taifex['RtData']['QuoteList']]
+futures_domestic["參考價"] = [i["CRefPrice"] for i in data_taifex['RtData']['QuoteList']]
+futures_domestic["時間"] = [i["CDate"] for i in data_taifex['RtData']['QuoteList']]
+
+df_future_dom = pd.DataFrame(futures_domestic)
+df_future_dom.to_csv("futures_domestic.csv",encoding='utf-8', index=False)
--- twse_scraper.py
+++ twse_scraper.py
@@ -1,4 +1,6 @@
+# 證交所twse
 import requests
+import pandas as pd
 from fake_useragent import UserAgent 
 
 ua = UserAgent()
@@ -7,7 +9,26 @@ headers = {
     'User-Agent': ua.random
 }
 
-url = 'https://www.twse.com.tw/rwd/zh/afterTrading/BWIBBU_d?date=20230331&selectType=24&response=json&_=1680575877167'
+url = 'https://www.twse.com.tw/rwd/zh/afterTrading/BWIBBU_d?date=20230331&selectType=24&response=json&_=1680575877170'
 response_twse = requests.get(url=url, headers=headers)
-data = response_twse.json()
-print(data['data'])
+data_twse = response_twse.json()
+
+code = [content_twse[0] for content_twse in data_twse['data']]
+titles = [content_twse[1] for content_twse in data_twse['data']]
+yield_ = [content_twse[2] for content_twse in data_twse['data']]
+dividend_year = [content_twse[3] for content_twse in data_twse['data']]
+pe_ratio = [content_twse[4] for content_twse in data_twse['data']]
+prb = [content_twse[5] for content_twse in data_twse['data']]
+financial_statement_yq = [content_twse[6] for content_twse in data_twse['data']]
+
+semi_con = {}
+semi_con["證券代號"]=code
+semi_con["股票名稱"]=titles
+semi_con["殖利率"]=yield_
+semi_con["股利年度"]=dividend_year
+semi_con["本益比"]=pe_ratio
+semi_con["股價淨值比"]=prb
+semi_con["財報年度年/季"]=financial_statement_yq
+
+df_semi = pd.DataFrame(semi_con)
+df_semi.to_csv("twse_semi_con.csv",encoding='utf-8', index=False)
